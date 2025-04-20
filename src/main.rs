d8ae15ead8f9c683f80b4bb7cd92f68bd78e889e
TimTheBig <132001783+TimTheBig@users.noreply.github.com>
2025-04-20 18:26:02 -0400

    Optimize code by using resource

--- src/main.rs
+++ src/main.rs
@@ -22,14 +22,26 @@ pub struct TransformCSGShapesEvent {
     pub transform: Transform,
 }
 
+#[derive(Resource)]
+struct SphereCSG(CSG);
+
+#[derive(Resource)]
+struct CubeCSG(CSG);
+
+#[derive(Resource)]
+struct CubeLastPos(Transform);
+
+#[derive(Component)]
+struct CubeMarker;
+
+#[derive(Component)]
+struct CSGCombined;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_plugins(TransformGizmoPlugin)
         .add_event::<TransformCSGShapesEvent>()
-        .add_systems(Startup, setup_scene)
-        .add_systems(Update, respawn_combined_csg_when_cube_moved)
         .insert_resource(GizmoOptions {
             hotkeys: Some(GizmoHotkeys::default()),
             group_targets: false,
@@ -49,6 +61,11 @@ fn main() {
             },
             ..default()
         })
+        .insert_resource(SphereCSG(CSG::sphere(SPHERE_RADIUS, 20, 12, None)))
+        .insert_resource(CubeCSG(CSG::cube(CUBE_SIZE, CUBE_SIZE, CUBE_SIZE, None).center()))
+        .insert_resource(CubeLastPos(Transform::default()))
+        .add_systems(Startup, setup_scene)
+        .add_systems(Update, respawn_combined_csg_when_cube_moved)
         .run();
 }
 
@@ -56,20 +73,17 @@ fn setup_scene(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
+    sphere_csg: Res<SphereCSG>,
+    cube_csg: Res<CubeCSG>,
 ) {
     let white_matl = materials.add(Color::WHITE);
     let ground_matl = materials.add(Color::Srgba(GRAY_300));
 
     // Spawn CSG Shapes
-    let cube_csg = CSG::cube(CUBE_SIZE, CUBE_SIZE, CUBE_SIZE, None).center();
-    let sphere_csg = CSG::sphere(SPHERE_RADIUS, 16, 8, None);
+    let cube_mesh_handle = meshes.add(csg_to_mesh(&cube_csg.0));
+    let sphere_mesh_handle = meshes.add(csg_to_mesh(&sphere_csg.0));
 
-    let cube_tesselation = cube_csg.tessellate();
-    let sphere_tesselation = sphere_csg.tessellate();
-
-    let cube_mesh_handle = meshes.add(csg_to_mesh(&cube_tesselation));
-    let sphere_mesh_handle = meshes.add(csg_to_mesh(&sphere_tesselation));
-
+    // cube
     commands.spawn((
         Mesh3d(cube_mesh_handle),
         MeshMaterial3d(white_matl.clone()),
@@ -78,11 +92,12 @@ fn setup_scene(
             2.0 + CSG_OFFSET_VALUE,
             0.0,
         ),
-        Name::new("cube".to_string()),
-        CSGShape { csg: cube_csg },
+        CubeMarker,
+        CSGShape { csg: cube_csg.0.clone() },
         GizmoTarget::default(),
     ));
 
+    // sphere
     commands.spawn((
         Mesh3d(sphere_mesh_handle),
         MeshMaterial3d(white_matl.clone()),
@@ -91,14 +106,13 @@ fn setup_scene(
             2.0 + CSG_OFFSET_VALUE,
             0.0,
         ),
-        Name::new("sphere".to_string()),
-        CSGShape { csg: sphere_csg },
-        // GizmoTarget::default(),
+        CSGShape { csg: sphere_csg.0.clone() },
+        GizmoTarget::default(),
     ));
 
     // Ground
     commands.spawn((
-        Mesh3d(meshes.add(Plane3d::default().mesh().size(50.0, 50.0).subdivisions(10))),
+        Mesh3d(meshes.add(Plane3d::default().mesh().size(50.0, 50.0).subdivisions(8))),
         MeshMaterial3d(ground_matl.clone()),
         PickingBehavior::IGNORE, 
     ));
@@ -126,60 +140,67 @@ fn setup_scene(
 fn respawn_combined_csg_when_cube_moved(
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
+    sphere_csg: Res<SphereCSG>,
+    cube_csg: Res<CubeCSG>,
+    mut cube_last_pos: ResMut<CubeLastPos>,
     mut commands: Commands,
-    csg_shape_query: Query<(Entity, &Name, &mut Transform, &mut CSGShape, &Mesh3d)>,
+    csg_combined_query: Option<Single<(Entity, &mut Transform, &mut CSGShape, &Mesh3d), (With<CSGCombined>, Without<CubeMarker>)>>,
+    csg_cube_entity: Single<(Entity, &mut Transform, &mut CSGShape, &Mesh3d), With<CubeMarker>>,
 ) {
-    for (entity, name, transform, mut _csg_shape, _mesh_handle) in csg_shape_query.iter() {
-        if name.as_str() == "CSG Combined" {
-            commands.entity(entity).despawn();
-        }
-
-        let cube_csg = CSG::cube(CUBE_SIZE, CUBE_SIZE, CUBE_SIZE, None).center();
-        let sphere_csg = CSG::sphere(SPHERE_RADIUS, 16, 8, None);
-
-        if name.as_str() == "cube" {
-            let translation = transform.translation;
-            let white_matl = materials.add(Color::WHITE);
-
-            let transformed_cube = cube_csg.translate(translation.x as f64, translation.y as f64, translation.z as f64);
-            //this is not right way to do it. there has to be a way to set the transform of the shape while creating it itself.
-            let transformed_sphere = sphere_csg.translate(0 as f64, 2.0 as f64, 0 as f64); 
-
-            let new_combined_csg = transformed_cube.difference(&transformed_sphere);
-
-            let new_tessellation = new_combined_csg.tessellate();
-            let new_mesh = csg_to_mesh(&new_tessellation);
-            let combined_mesh_handle = meshes.add(new_mesh);
-
-            commands.spawn((
-                Mesh3d(combined_mesh_handle),
-                MeshMaterial3d(white_matl.clone()),
-                Transform::from_xyz(0.0, CSG_COMBINED_OFFSET_VALUE, 0.0),
-                Name::new("CSG Combined".to_string()),
-                CSGShape { csg: new_combined_csg },
-            ));
-        }
+    // skip if cube did not move
+    if *csg_cube_entity.1 == cube_last_pos.0 {
+        return;
     }
+
+    if let Some(csg_combined_entity) = csg_combined_query {
+        commands.entity(csg_combined_entity.0).despawn();
+    }
+
+    let transform = *csg_cube_entity.1;
+
+    cube_last_pos.0 = transform;
+    let translation = transform.translation;
+    let white_matl = materials.add(Color::WHITE); // todo put in res
+
+    let transformed_cube = &cube_csg.0.translate(translation.x as f64, translation.y as f64, translation.z as f64);
+    // this is not right way to do it. there has to be a way to set the transform of the shape while creating it itself.
+    let transformed_sphere = &sphere_csg.0.translate(0f64, 2f64, 0f64); 
+
+    let new_combined_csg = transformed_cube.difference(transformed_sphere);
+
+    let new_mesh = csg_to_mesh(&new_combined_csg);
+    let combined_mesh_handle = meshes.add(new_mesh);
+
+    commands.spawn((
+        Mesh3d(combined_mesh_handle),
+        MeshMaterial3d(white_matl.clone()),
+        transform,
+        // Transform::from_xyz(0.0, CSG_COMBINED_OFFSET_VALUE, 0.0),
+        CSGCombined,
+        CSGShape { csg: new_combined_csg },
+    ));
 }
 
+/// This tessellates the `CSG` then convert it to a [`Mesh`]
 fn csg_to_mesh(csg: &CSG<()>) -> Mesh {
-    let polygons = &csg.polygons;
+    let polygons = &csg.tessellate().unwrap().polygons;
 
     // Prepare buffers
-    let mut positions_32 = Vec::new();
-    let mut normals_32   = Vec::new();
-    let mut indices      = Vec::new();
+    let vertices_len = polygons.iter().map(|poly| poly.vertices.len()).sum();
+    let mut positions_32 = Vec::with_capacity(vertices_len);
+    let mut normals_32   = Vec::with_capacity(vertices_len);
+    let mut indices = Vec::with_capacity(vertices_len);
 
     let mut index_start = 0u32;
 
     // Each polygon is assumed to have exactly 3 vertices after tessellation.
-    // If not, be sure to handle polygons with more than 3 vertices, or call `csg.tessellate()`.
     for poly in polygons {
         // skip any degenerate polygons
         if poly.vertices.len() != 3 {
             continue;
         }
 
+        debug_assert!(poly.vertices.len() == 3);
         // push 3 positions/normals
         for v in &poly.vertices {
             positions_32.push([v.pos.x as f32, v.pos.y as f32, v.pos.z as f32]);
