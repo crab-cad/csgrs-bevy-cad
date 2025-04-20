5e3b185543e1801aef4f68969e025f7e1f300667
TimTheBig <132001783+TimTheBig@users.noreply.github.com>
2025-04-20 19:54:14 -0400

    Dubble scale

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use bevy::{asset::RenderAssetUsages, color::palettes::tailwind::*, prelude::*, render::mesh::{Indices, PrimitiveTopology}};
+use bevy::{asset::RenderAssetUsages, prelude::*, render::mesh::{Indices, PrimitiveTopology}};
 use csgrs::csg::CSG;
 use transform_gizmo_bevy::{prelude::*, GizmoHotkeys};
 
@@ -7,8 +7,8 @@ const CSG_OFFSET_VALUE: f32 = 0.0;
 const CSG_COMBINED_OFFSET_VALUE: f32 = 8.0;
 const SHAPES_X_EXTENT: f32 = 7.0;
 
-const CUBE_SIZE: f64 = 2.0;
-const SPHERE_RADIUS: f64 = 1.0;
+const CUBE_SIZE: f64 = 4.0;
+const SPHERE_RADIUS: f64 = 2.0;
 
 #[derive(Component)]
 struct CSGShape {
@@ -23,13 +23,16 @@ pub struct TransformCSGShapesEvent {
 }
 
 #[derive(Resource)]
-struct SphereCSG(CSG);
+struct SphereCSG(CSG<()>);
 
 #[derive(Resource)]
-struct CubeCSG(CSG);
+struct CubeCSG(CSG<()>);
 
 #[derive(Resource)]
 struct CubeLastPos(Transform);
+
+#[derive(Resource)]
+struct CombinedCSGMaterial(Handle<StandardMaterial>);
 
 #[derive(Component)]
 struct CubeMarker;
@@ -41,6 +44,7 @@ fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_plugins(TransformGizmoPlugin)
+        .add_plugins(bevy_editor_cam::DefaultEditorCamPlugins)
         .add_event::<TransformCSGShapesEvent>()
         .insert_resource(GizmoOptions {
             hotkeys: Some(GizmoHotkeys::default()),
@@ -59,6 +63,7 @@ fn main() {
                 modes.insert(GizmoMode::RotateZ);
                 modes
             },
+            visuals: GizmoVisuals { inactive_alpha: 0.1, ..default() },
             ..default()
         })
         .insert_resource(SphereCSG(CSG::sphere(SPHERE_RADIUS, 20, 12, None)))
@@ -66,6 +71,7 @@ fn main() {
         .insert_resource(CubeLastPos(Transform::default()))
         .add_systems(Startup, setup_scene)
         .add_systems(Update, respawn_combined_csg_when_cube_moved)
+        .add_systems(Update, toggle_bottom_light)
         .run();
 }
 
@@ -76,8 +82,19 @@ fn setup_scene(
     sphere_csg: Res<SphereCSG>,
     cube_csg: Res<CubeCSG>,
 ) {
-    let white_matl = materials.add(Color::WHITE);
-    let ground_matl = materials.add(Color::Srgba(GRAY_300));
+    let shape_mat = materials.add(Color::WHITE);
+    let ground_mat = materials.add(StandardMaterial {
+        base_color: Color::Srgba(bevy::color::palettes::tailwind::GRAY_300),
+        perceptual_roughness: 0.6,
+        thickness: 1.0,
+        ior: 1.6,
+        reflectance: 0.55,
+        diffuse_transmission: 0.25,
+        ..default()
+    });
+    commands.insert_resource(
+        CombinedCSGMaterial(materials.add(const { Color::srgb(0.0, 0.0, 0.9) }))
+    );
 
     // Spawn CSG Shapes
     let cube_mesh_handle = meshes.add(csg_to_mesh(&cube_csg.0));
@@ -86,10 +103,10 @@ fn setup_scene(
     // cube
     commands.spawn((
         Mesh3d(cube_mesh_handle),
-        MeshMaterial3d(white_matl.clone()),
+        MeshMaterial3d(shape_mat.clone()),
         Transform::from_xyz(
-            -3.0,
-            2.0 + CSG_OFFSET_VALUE,
+            -6.0,
+            4.0 + CSG_OFFSET_VALUE,
             0.0,
         ),
         CubeMarker,
@@ -100,10 +117,10 @@ fn setup_scene(
     // sphere
     commands.spawn((
         Mesh3d(sphere_mesh_handle),
-        MeshMaterial3d(white_matl.clone()),
+        MeshMaterial3d(shape_mat.clone()),
         Transform::from_xyz(
             0.0,
-            2.0 + CSG_OFFSET_VALUE,
+            4.0 + CSG_OFFSET_VALUE,
             0.0,
         ),
         CSGShape { csg: sphere_csg.0.clone() },
@@ -112,8 +129,8 @@ fn setup_scene(
 
     // Ground
     commands.spawn((
-        Mesh3d(meshes.add(Plane3d::default().mesh().size(50.0, 50.0).subdivisions(8))),
-        MeshMaterial3d(ground_matl.clone()),
+        Mesh3d(meshes.add(Plane3d::default().mesh().size(128.0, 128.0).subdivisions(8))),
+        MeshMaterial3d(ground_mat),
         PickingBehavior::IGNORE, 
     ));
 
@@ -121,27 +138,65 @@ fn setup_scene(
     commands.spawn((
         PointLight {
             shadows_enabled: true,
-            intensity: 10_000_000.,
-            range: 100.0,
+            intensity: 30_000_000.,
+            range: 110.0,
             shadow_depth_bias: 0.2,
             ..default()
         },
-        Transform::from_xyz(8.0, 16.0, 8.0),
+        Transform::from_xyz(14.0, 32.0, 14.0),
+    ));
+    commands.spawn((
+        PointLight {
+            shadows_enabled: false,
+            intensity: 25_000_000.,
+            range: 100.0,
+            ..default()
+        },
+        Transform::from_xyz(0.0, -30.0, 0.0),
     ));
 
     // Camera
     commands.spawn((
         Camera3d::default(),
-        Transform::from_xyz(7.0, 13.0, 30.0).looking_at(Vec3::new(0., 1., 0.), Vec3::Y),
+        Transform::from_xyz(12.0, 22.0, 45.0).looking_at(Vec3::new(0., 1., 0.), Vec3::Y),
         GizmoCamera,
+        bevy_editor_cam::controller::component::EditorCam {
+            zoom_limits: bevy_editor_cam::prelude::zoom::ZoomLimits { min_size_per_pixel: 1e-6, max_size_per_pixel: 0.5, zoom_through_objects: false },
+            enabled_motion: bevy_editor_cam::prelude::EnabledMotion { pan: true, orbit: false, zoom: true },
+            sensitivity: bevy_editor_cam::prelude::Sensitivity { orbit: Vec2::ZERO, zoom: 2.0 },
+            ..default()
+        },
     ));
+}
+
+fn toggle_bottom_light(light_query: Query<(Entity, &PointLight, &Transform)>, keyboard_events: Res<ButtonInput<KeyCode>>, mut commands: Commands) {
+    if keyboard_events.just_pressed(KeyCode::Space) {
+        // toggle any lights with negative heights
+        // sorry just don't even read this please
+        if !(light_query.iter().any(|(entity, _, transform)| {
+            if transform.translation.y < 0.0 {
+                commands.entity(entity).despawn();
+                true
+            } else { false }
+        })) {
+            commands.spawn((
+                PointLight {
+                    shadows_enabled: false,
+                    intensity: 25_000_000.,
+                    range: 100.0,
+                    ..default()
+                },
+                Transform::from_xyz(0.0, -30.0, 0.0),
+            ));
+        }
+    }
 }
 
 fn respawn_combined_csg_when_cube_moved(
     mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<StandardMaterial>>,
     sphere_csg: Res<SphereCSG>,
     cube_csg: Res<CubeCSG>,
+    combined_csg_material: Res<CombinedCSGMaterial>,
     mut cube_last_pos: ResMut<CubeLastPos>,
     mut commands: Commands,
     csg_combined_query: Option<Single<(Entity, &mut Transform, &mut CSGShape, &Mesh3d), (With<CSGCombined>, Without<CubeMarker>)>>,
@@ -160,11 +215,10 @@ fn respawn_combined_csg_when_cube_moved(
 
     cube_last_pos.0 = transform;
     let translation = transform.translation;
-    let white_matl = materials.add(Color::WHITE); // todo put in res
 
     let transformed_cube = &cube_csg.0.translate(translation.x as f64, translation.y as f64, translation.z as f64);
     // this is not right way to do it. there has to be a way to set the transform of the shape while creating it itself.
-    let transformed_sphere = &sphere_csg.0.translate(0f64, 2f64, 0f64); 
+    let transformed_sphere = &sphere_csg.0.translate(2.0, 5.0, 0.0); 
 
     let new_combined_csg = transformed_cube.difference(transformed_sphere);
 
@@ -173,7 +227,7 @@ fn respawn_combined_csg_when_cube_moved(
 
     commands.spawn((
         Mesh3d(combined_mesh_handle),
-        MeshMaterial3d(white_matl.clone()),
+        MeshMaterial3d(combined_csg_material.0.clone_weak()),
         transform,
         // Transform::from_xyz(0.0, CSG_COMBINED_OFFSET_VALUE, 0.0),
         CSGCombined,
@@ -200,7 +254,6 @@ fn csg_to_mesh(csg: &CSG<()>) -> Mesh {
             continue;
         }
 
-        debug_assert!(poly.vertices.len() == 3);
         // push 3 positions/normals
         for v in &poly.vertices {
             positions_32.push([v.pos.x as f32, v.pos.y as f32, v.pos.z as f32]);
